f913176a4195fe57f6d056bca88e8e38cd43244e
y.savchenko <y.savchenko@arkadium.com>
2018-07-18 15:31:23 +0300

    add script for check

---
+++ check-exist.ps1
@@ -0,0 +1,9 @@
+$RG = $env:RGname
+$WebAppName = $env:appName
+$Function = Get-AzureRmWebApp -ResourceGroupName $RG -Name $WebAppName -ErrorAction SilentlyContinue
+
+if ($Function -eq $null)
+    {Write-Verbose "The function isn't exist, let's create it" -Verbose}
+else
+    {Write-Error -Message "The function is already exist, don't touch it!!!"}
+
